f6a419df286ec43a2dc5e9dbd8d827922032505c
Rares <rsahleanu@gmail.com>
2020-11-23 23:44:34 +0100

    [NeuronalNetwork function convertion]

--- API/src/main/java/de/rares/visnet/api/neuronalnetwork/normal/NormalNeuronalNetwork.java
+++ API/src/main/java/de/rares/visnet/api/neuronalnetwork/normal/NormalNeuronalNetwork.java
@@ -1,6 +1,12 @@
 package de.rares.visnet.api.neuronalnetwork.normal;
 
+import de.rares.visnet.api.math.function.Function;
+import de.rares.visnet.api.math.structures.vector.Vector;
 import de.rares.visnet.api.neuronalnetwork.normal.element.layer.Layer;
+import de.rares.visnet.api.neuronalnetwork.normal.element.neuron.Neuron;
+
+
+
 
 public class NormalNeuronalNetwork {
 
@@ -9,11 +15,32 @@ public class NormalNeuronalNetwork {
     public Layer[] structure;
     public int outputDimension;
     public int inputDimension;
-
+    public Function nnFunction;
     public NormalNeuronalNetwork(Layer[] structure) {
         this.structure = structure;
         outputDimension = structure[structure.length -1].dimension;
         inputDimension = structure[0].dimension;
     }
+
+
+
+    public void convertToFunction(int layerPos){
+
+
+            Layer layer = structure[layerPos];
+
+            for (int nPos = 0; nPos < layer.dimension; nPos++ ){
+
+                Neuron n = layer.neurons[nPos];
+                Vector<Double> neuronWeights = layer.weights[nPos];
+                                
+            }
+
+    }
+
+
+
+
+
 
 }

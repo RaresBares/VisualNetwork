eeb0648f91527c72d252eb4d19373bab7b76412c
Rares <rsahleanu@gmail.com>
2020-11-29 20:10:50 +0100

    [Implemented multiple variable derivation] [fixxed bugs] [Structured Derivationsegment] [performance improvements]

--- API/src/main/java/de/rares/visnet/api/VisNetApi.java
+++ API/src/main/java/de/rares/visnet/api/VisNetApi.java
@@ -1,7 +1,7 @@
 package de.rares.visnet.api;
 
 
-import de.rares.visnet.api.math.diff.derivative.patterns.Dericative;
+import de.rares.visnet.api.math.derivative.derivative.Derivative;
 
 
 public class VisNetApi {
@@ -13,26 +13,19 @@ public class VisNetApi {
     }
 
     public static void main(String[] args) {
-       /* Layer layer1 = new Layer(2, new ReLU());
-        Layer layer2 = new Layer(2, new ReLU());
-        Layer layer3 = new Layer(2, new ReLU());
-        NormalNeuronalNetwork network = new NormalNeuronalNetwork(new Layer[]{layer1, layer2, layer3});
-        String[] res = network.convertToFunction();
-        for (String re : res) {
-            System.out.println(re);
-        }*/
 
 
 
-      String function = "((2) * (x)^(3) + 3)^(5)".replace(" ", "");
-        Dericative d = new Dericative(function);
 
-
-       System.out.println("Ableitung: " + d.getDerivative());
+      String function = "x * y".replace(" ", "");
 
 
 
-      //  DifferentialCalc diff = new DifferentialCalc(function, new String[]{"x"});
+       System.out.println("Ableitung: " + new Derivative(function).getDerivative("y"));
+
+
+
+
     }
 
 
---
+++ API/src/main/java/de/rares/visnet/api/math/derivative/derivative/DerivationManager.java
@@ -0,0 +1,26 @@
+package de.rares.visnet.api.math.derivative.derivative;
+
+public class DerivationManager {
+
+    private final Derivative derivative;
+
+
+
+    public DerivationManager(String function){
+
+        derivative = new Derivative(function);
+    }
+
+    public static String clearClauses(String func){
+        return Derivative.trimClauses(func,true);
+    }
+
+    public String derive(String refer){
+
+        return derivative.getDerivative(refer);
+
+    }
+
+
+
+}
